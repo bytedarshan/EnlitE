fb65c6464751d92435a17c6f0b13baf698317edc
Darshan <darshan.challani18@gmail.com>
2026-04-12 14:36:25 +0530

    analytics/next"

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css' // THIS LINE IS CRITICAL
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react" // <-- THIS MUST BE 'react', NOT 'next'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Analytics/> {/* Add it right here, below App */}
+    <Analytics/> 
   </StrictMode>,
 )
